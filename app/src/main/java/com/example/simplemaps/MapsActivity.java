0e991b6922c68f3193c3ad0fec62d2fc95aff0fb
irshadrasyidi <didot.brodot@yahoo.com>
2019-09-19 23:22:34 +0700

    update

--- app/src/main/java/com/example/simplemaps/MapsActivity.java
+++ app/src/main/java/com/example/simplemaps/MapsActivity.java
@@ -1,6 +1,5 @@
 package com.example.simplemaps;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
@@ -11,12 +10,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.InputMethod;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -28,6 +27,23 @@ import java.io.IOException;
 import java.util.List;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.normal : mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL); break;
+            case R.id.terrain: mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN); break;
+            case R.id.satellite : mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE); break;
+            case R.id.hybrid: mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID); break;
+            case R.id.none:mMap.setMapType(GoogleMap.MAP_TYPE_NONE); break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     private GoogleMap mMap;
 
@@ -40,10 +56,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        Button go = (Button) findViewById(R.id.idGo);
+        Button go = findViewById(R.id.idGo);
         go.setOnClickListener(op);
-    }
 
+        Button cari = (Button) findViewById(R.id.idCari);
+        cari.setOnClickListener(op);
+    }
 
     /**
      * Manipulates the map once available.
@@ -57,61 +75,22 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
         LatLng ITS = new LatLng(-7.2819705, 112.795323);
         mMap.addMarker(new MarkerOptions().position(ITS).title("Marker in ITS"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ITS, 15));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ITS,15));
     }
 
-    private void gotoPeta(Double lat, Double lng, float z){
-        LatLng Lokasibaru = new LatLng(lat, lng);
-        mMap.addMarker(new MarkerOptions().position(Lokasibaru).title("Marker in " + lat + ":" + lng));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Lokasibaru, z));
-    }
-
-    View.OnClickListener op = new View.OnClickListener(){
+    View.OnClickListener op = new View.OnClickListener() {
         @Override
-        public void onClick(View view){
-            switch (view.getId()){
-                case R.id.idGo:sembunyikanKeyBoard(view);
-                gotoLokasi(); break;
-                case R.id.idCari:sembunyikanKeyBoard(view);
-                goCari(); break;
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.idGo:sembunyikanKeyBoard(v);
+                    gotoLokasi();break;
+                case R.id.idCari:goCari();break;
             }
         }
     };
-
-    private void goCari(){
-        EditText tempat = (EditText) findViewById(R.id.idDaerah);
-        Geocoder g = new Geocoder(getBaseContext());
-        try{
-            List<android.location.Address> daftar = g.getFromLocationName(tempat.getText().toString(), 1);
-            Address alamat = daftar.get(0);
-
-            String nemuAlamat = alamat.getAddressLine(0);
-            Double lintang = alamat.getLatitude();
-            Double bujur = alamat.getLongitude();
-
-            Toast.makeText(getBaseContext(), "Ketemu " + nemuAlamat, Toast.LENGTH_LONG).show();
-
-            EditText zoom = (EditText) findViewById(R.id.idZoom);
-            Float dblzoom = Float.parseFloat(zoom.getText().toString());
-            Toast.makeText(this, "Move to " + nemuAlamat + "Lat:" + lintang + " Long:" + bujur, Toast.LENGTH_LONG).show();
-            gotoPeta(lintang, bujur, dblzoom);
-
-            EditText lat = (EditText) findViewById(R.id.idLokasiLat);
-            EditText lng = (EditText) findViewById(R.id.idLokasiLng);
-            Double dbllat = Double.parseDouble(lat.getText().toString());
-            Double dbllng = Double.parseDouble(lng.getText().toString());
-            hitungJarak(dbllat, dbllng, lintang, bujur);
-
-            lat.setText(lintang.toString());
-            lng.setText(bujur.toString());
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
 
     private void gotoLokasi(){
         EditText lat = (EditText) findViewById(R.id.idLokasiLat);
@@ -121,35 +100,55 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         Double dbllat = Double.parseDouble(lat.getText().toString());
         Double dbllng = Double.parseDouble(lng.getText().toString());
         Float dblzoom = Float.parseFloat(zoom.getText().toString());
-
-        Toast.makeText(this, "Move to Lat:" + dbllat + " Long:" + dbllng, Toast.LENGTH_SHORT).show();
-        gotoPeta(dbllat, dbllng, dblzoom);
+        Toast.makeText(this, "Move to Lat:"+dbllat+" Long:"+dbllng, Toast.LENGTH_LONG)
+                .show();
+        gotoPeta(dbllat,dbllng, dblzoom);
     }
 
     private void sembunyikanKeyBoard(View v){
         InputMethodManager a = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        a.hideSoftInputFromWindow(v.getWindowToken(), 0);
+        a.hideSoftInputFromWindow(v.getWindowToken(),0);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
+    private void gotoPeta(Double lat, Double lng, float z){
+        LatLng Lokasibaru = new LatLng(lat, lng);
+        mMap.addMarker(new MarkerOptions().position(Lokasibaru).title("Marker in " + lat + ":" + lng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Lokasibaru,z));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()){
-            case R.id.normal : mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL); break;
-            case R.id.terrain : mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN); break;
-            case R.id.sattelite : mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE); break;
-            case R.id.hybrid : mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID); break;
-            case R.id.none : mMap.setMapType(GoogleMap.MAP_TYPE_NONE); break;
+    private void goCari(){
+        EditText tempat = (EditText) findViewById(R.id.idDaerah);
+        Geocoder g =new Geocoder(getBaseContext());
+        try {
+            List<android.location.Address> daftar = g.getFromLocationName(tempat.getText().toString(), 1);
+            Address alamat = daftar.get(0);
+
+            String nemuAlamat = alamat.getAddressLine(0);
+            Double lintang = alamat.getLatitude();
+            Double bujur = alamat.getLongitude();
+
+            Toast.makeText(getBaseContext(), "Ketemu" + nemuAlamat, Toast.LENGTH_LONG).show();
+
+            EditText zoom = (EditText) findViewById(R.id.idZoom);
+            Float dblzoom = Float.parseFloat(zoom.getText().toString());
+            Toast.makeText(this, "Move to" + nemuAlamat + " Lat:" + lintang + " Long:" +bujur, Toast.LENGTH_LONG).show();
+            gotoPeta(lintang, bujur, dblzoom);
+
+            EditText lat = (EditText) findViewById(R.id.idLokasiLat);
+            EditText lng = (EditText) findViewById(R.id.idLokasiLng);
+            Double dbllat = Double.parseDouble(lat.getText().toString());
+            Double dbllng = Double.parseDouble(lng.getText().toString());
+            hitungJarak(dbllat, dbllng, lintang, bujur);
+            lat.setText(lintang.toString());
+            lng.setText(bujur.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return super.onOptionsItemSelected(item);
+
     }
 
-    private void hitungJarak(double latAsal, Double lngAsal, double latTujuan, double lngTujuan){
+    private void hitungJarak(double latAsal, double lngAsal, double latTujuan, double lngTujuan){
         Location asal = new Location("asal");
         Location tujuan = new Location("tujuan");
         tujuan.setLatitude(latTujuan);
@@ -161,3 +160,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         Toast.makeText(getBaseContext(), "jarak : " + jaraknya + " km ", Toast.LENGTH_LONG).show();
     }
 }
+
+
+
